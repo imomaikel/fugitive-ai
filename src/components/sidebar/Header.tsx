5514835d4115b4a6ccc66e01fbf970182d5bd2ca
Michał Kołodziejczyk <imomaikeldev@gmail.com>
2025-04-05 23:36:25 +0200

    Improved sidebar header

--- src/components/sidebar/Header.tsx
+++ src/components/sidebar/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { SidebarMenu, SidebarMenuItem } from '../ui/sidebar';
 
@@ -6,13 +7,22 @@ const SidebarHeaderContent = () => {
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <div className="hover:bg-primary-foreground flex items-center overflow-hidden rounded-md transition-colors">
-          <Image width={64} height={64} src="/logo-no-bg.webp" alt="Logo" className="max-size-[64px] object-contain" />
-          <div className="pointer-events-none flex flex-1 flex-col items-center justify-center whitespace-nowrap data-[state=open]:hidden">
-            <h1 className="text-xl font-semibold">Fugitive AI</h1>
-            <p className="text-muted-foreground text-xs">Track fugitives</p>
+        <Link href="/platform">
+          <div className="hover:bg-primary-foreground flex max-h-[64px] items-center overflow-hidden rounded-md transition-colors">
+            <Image
+              width={64}
+              height={64}
+              src="/logo-no-bg.webp"
+              alt="Logo"
+              className="aspect-square max-h-[64px] min-w-[30px]"
+              loading="eager"
+            />
+            <div className="pointer-events-none flex flex-1 flex-col items-center justify-center whitespace-nowrap data-[state=open]:hidden">
+              <h1 className="text-xl font-semibold">Fugitive AI</h1>
+              <p className="text-muted-foreground text-xs">Track fugitives</p>
+            </div>
           </div>
-        </div>
+        </Link>
       </SidebarMenuItem>
     </SidebarMenu>
   );
