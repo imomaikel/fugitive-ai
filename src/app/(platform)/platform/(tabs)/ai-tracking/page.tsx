11e55b0d28459e454d502ad8a7e12a925790d4e1
Michał Kołodziejczyk <imomaikeldev@gmail.com>
2025-04-26 21:54:11 +0200

    Added AI Tracking tab

---
+++ src/app/(platform)/platform/(tabs)/ai-tracking/page.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from '../../_components/PageWrapper';
+
+const AiTrackingPage = async () => {
+  return (
+    <PageWrapper pageName="AI Tracking">
+      <div></div>
+    </PageWrapper>
+  );
+};
+
+export default AiTrackingPage;
